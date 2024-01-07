2afed69e1551b2cbdcb1d71e8789a3217b04b988
syed <syedmujtabaarshasd772@gmail.com>
2024-01-07 14:12:16 +0500

    practce set

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//    function table(number){
+//     for( let i=1;i<=10;i++){
+//         let res= i*number;
+//         // console.log(`${number} X ${i} = ${res} `);
+//         console.log(`${number}X ${i} = ${res}`);
+//     }
+//    }
+//     table(5);
+
+        
