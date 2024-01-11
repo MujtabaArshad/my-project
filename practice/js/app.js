4906cf88315a5cfdeaf7377b8badc931b5dd21a3
syed <syedmujtabaarshasd772@gmail.com>
2024-01-12 04:12:02 +0500

    tic Tack Game complete

--- practice/js/app.js
+++ practice/js/app.js
@@ -1,13 +1,28 @@
+
+
+
+
 let boxes=document.querySelectorAll('.box');
+
+
+
 let reset=document.querySelector('#reset-btn');
 let newGameBtn=document.querySelector('#new-btn');
 
 let msgContainer=document.querySelector('.msg-container');
+
 let msg=document.querySelector('#msg');
+msg.innerHTML="";
+
 let turnO=true;
+
 const winPatterns=[
+
+
 [0, 1, 2],
+
 [0, 3, 6],
+
 [0, 4, 8],
 [1, 4, 7],
 [2, 5, 8],
@@ -16,12 +31,19 @@ const winPatterns=[
 [6, 7, 8],
 
 ];
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+}
+
+
 boxes.forEach((box)=>{
 box.addEventListener('click',()=>{
-
 if(turnO){
 box.innerText='0';
 turnO=false;
+
 
 }
 else{
@@ -29,34 +51,57 @@ box.innerText='x';
 turnO=true;
 }
 box.disabled=true;
-
- checkWinner();
+checkWinner();
 });
 });
 
-const showWinner =(winner)=>{
-    msg.innerText=`game winner ${winner}`;
+
+// let showWinner=(winner) =>{
+//     msg.innerText=`Game winner${winner}`;
+//     msgContainer.classList.remove('hide');
+
+// }           
+
+let disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+
+    }
+};
+
+let enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerHTML="";
+
+    }
+};
+
+const showWinner=(winner)=>{
+    msg.innerText=`Game winner : ${winner}`;
     msgContainer.classList.remove('hide');
-
+    disableBoxes();
 }
-
 const checkWinner=()=>{
-for(pattern of winPatterns){
+for(let pattern of winPatterns){
+
 let pos1Val=boxes[pattern[0]].innerText;
 let pos2Val=boxes[pattern[1]].innerText;
 let pos3Val=boxes[pattern[2]].innerText;
-if(pos1Val!="" && pos2Val!=""&& pos3Val !=""){
 
-if(pos1Val=== pos2Val && pos2Val ===pos3Val)
-console.log('winner',pos1Val);
-showWinner(pos1Val);
+if(pos1Val!="" &&pos2Val!=""&&pos2Val!=""&&pos3Val){
+if(pos1Val===pos2Val &&pos2Val===pos3Val){
 
+showWinner(pos1Val)
+return;
 }
+}
+}
+};
 
-
+newGameBtn.addEventListener('click',resetGame);
+reset.addEventListener('click',resetGame);
 
 
 // console.log(pattern[0],pattern[1],pattern[2]);
 // console.log( boxes [pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
-}
-};
